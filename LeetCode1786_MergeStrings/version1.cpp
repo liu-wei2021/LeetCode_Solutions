4f1d2bdedcb5544b118f663a24cca7c60081fdc1
liu-wei2021 <e0564178@u.nus.edu>
2024-09-05 21:26:25 -0500

    Add version 1 of merge string solution

---
+++ LeetCode1786_MergeStrings/version1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+
+
+public:
+    std::string mergeAlternately(string word1, string word2) {
+        std::string merged_string = "";
+        int min_length = std::min(word1.length(),word2.length());
+
+        for (int i = 0; i < min_length; i++){
+            merged_string += word1[i];
+            merged_string += word2[i];
+        }
+
+        if (word1.length() > word2.length()){
+            merged_string += word1.substr(min_length);
+        }
+        
+        if (word2.length() > word1.length()){
+            merged_string += word2.substr(min_length);
+        }
+
+        return merged_string;
+
+    }
+};
